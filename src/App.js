f97343d33c56b282e7c602cfb710ad535baff2f4
pathas <pathas@naver.com>
2020-03-02 00:16:34 +0900

    do-it-01

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './sass/materialize.scss';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <div>두잇! 리액트 시작하기</div>
+          </div>
+        </nav>
+        <h1>머티리얼 CSS</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
