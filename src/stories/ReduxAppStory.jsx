0757ec9ba19a922cc5c72c5cb30a85e851d05740
pathas <pathas@naver.com>
2020-03-05 17:33:32 +0900

    Start Redux

---
+++ src/stories/ReduxAppStory.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import ReduxApp01 from '../07/ReduxApp01';
+
+storiesOf('ReduxApp01', module).addWithJSX('Default Settings', () => <ReduxApp01 />);
